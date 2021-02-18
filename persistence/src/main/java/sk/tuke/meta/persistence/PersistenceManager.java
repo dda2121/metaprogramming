5370771f06fa4acb96f2fbb542d9cb80b2a43217
Sergej Chodarev <sergejx@sergejx.net>
2021-02-18 12:10:19 +0100

    Initial commit

---
+++ persistence/src/main/java/sk/tuke/meta/persistence/PersistenceManager.java
@@ -0,0 +1,64 @@
+package sk.tuke.meta.persistence;
+
+import java.util.List;
+
+/**
+ * PersistenceManager allows to persist a set of entities into a database.
+ * <p>
+ * All methods of the interface may throw
+ * {@link javax.persistence.PersistenceException}.
+ * <p>
+ * Implementations of this interface may require additional configuration
+ * as constructor arguments, e.g. database connection.
+ */
+public interface PersistenceManager {
+
+    /**
+     * Create database tables for all managed entity classes.
+     */
+    void createTables();
+
+    /**
+     * Get all entities of specified type.
+     *
+     * @param clazz entity class
+     * @return a list of all entities stored in the database.
+     */
+    <T> List<T> getAll(Class<T> clazz);
+
+    /**
+     * Get a specific entity based on the primary key.
+     *
+     * @param type entity class
+     * @param id   primary key (id) value
+     * @return the found entity or <code>null</code> if the entity does not exist
+     */
+    <T> T get(Class<T> type, int id);
+
+    /**
+     * Get entities based on any field value.
+     *
+     * @param type      entity class
+     * @param fieldName name of the field
+     * @param value     searched field value
+     * @return a list of entities where named field has specified value
+     */
+    <T> List<T> getBy(Class<T> type, String fieldName, Object value);
+
+    /**
+     * Save entity into a database.
+     * If entity has a non-zero identifier, manager would try to perform
+     * <code>UPDATE</code>, otherwise <code>INSERT</code> would be performed.
+     *
+     * @param entity the entity to be saved
+     * @return the value of primary key
+     */
+    int save(Object entity);
+
+    /**
+     * Delete the entity from the database.
+     *
+     * @param entity the entity to be deleted
+     */
+    void delete(Object entity);
+}
---
+++ persistence/src/main/java/sk/tuke/meta/persistence/ReflectivePersistenceManager.java
@@ -0,0 +1,39 @@
+package sk.tuke.meta.persistence;
+
+import java.sql.Connection;
+import java.util.Collections;
+import java.util.List;
+
+public class ReflectivePersistenceManager implements PersistenceManager {
+
+    public ReflectivePersistenceManager(Connection connection, Class<?>... entities) {
+    }
+
+    @Override
+    public void createTables() {
+    }
+
+    @Override
+    public <T> List<T> getAll(Class<T> clazz) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public <T> T get(Class<T> type, int id) {
+        return null;
+    }
+
+    @Override
+    public <T> List<T> getBy(Class<T> type, String fieldName, Object value) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public int save(Object entity) {
+        return 0;
+    }
+
+    @Override
+    public void delete(Object entity) {
+    }
+}
