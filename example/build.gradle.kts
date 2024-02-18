f596928f96b969455bf6c9d82716e07901fab84b
Sergej Chodarev <sergejx@sergejx.net>
2024-02-18 15:28:06 +0100

    Convert project skeleton from Maven to Gradle

---
+++ example/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("application")
+}
+
+group = "sk.tuke.meta"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":persistence"))
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "sk.tuke.meta.example.Main"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
